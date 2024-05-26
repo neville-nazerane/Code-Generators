63a4434936b9060c2ef1f46e46af6e088d26971c
Neville <live.neville@live.com>
2024-05-26 14:49:51 -0700

    maui single project

---
+++ src/Maui/MauiSingleProectSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiSingleProectSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
